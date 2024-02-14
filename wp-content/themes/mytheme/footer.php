671f3c8f907d1c48be0fa86c132d6c0eed79d042
Erik Georgievski <erik.georgievski@gritacademy.se>
2024-02-14 21:56:07 +0100

    Done with all the content in the footer

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -16,24 +16,27 @@
 
         </div>
     </div>
+    <div class="footer-container">
     <div class="footer-left">
-    </div>
-
-    <div class="footer-center">
         <div class="store-info">
-        
-        <p>
-             <?php echo get_bloginfo('name'); ?>
-            <?php echo get_option('woocommerce_store_city') . "," ?> <br>
-            <?php echo get_option('woocommerce_store_postcode'); ?>
-            <?php echo get_option('woocommerce_default_country'); ?>
 
-        </p>
+            <p>
+                <?php echo get_bloginfo('name'); ?>
+                <?php echo get_option('woocommerce_store_city') . "," ?> <br>
+                <?php echo get_option('woocommerce_store_postcode'); ?>
+                <?php echo get_option('woocommerce_default_country'); ?>
+
+            </p>
 
 
 
         </div>
+
+
+    </div>
+    <div class="footer-center1">
         <?php
+        echo "<div class='footerheader1'>Links</div>";
 
         $menu_header = array(
             'theme_location' => 'huvudmeny',
@@ -44,13 +47,15 @@
         wp_nav_menu($menu_header);
         ?>
 
-    </div>
 
-    <div class="footer-right">
+
+        </div>
+
+    <div class="footer-center2">
         <?php
 
 
-
+        echo "<div class='footerheader2'>Help</div>";
         $menu_header = array(
             'theme_location' => 'footer_meny',
             'menu_id' => 'footer-menu',
@@ -61,6 +66,20 @@
         ?>
 
     </div>
+    <div class="footer-right">
+        <div class="footerheader3">Newsletter</div>
+        <div class="footer-form">
+            <input type="text">
+            <button>Subscribe</button>
+        </div>
+
+    </div>
+
+
+
+    </div>
+   
+   
 
     <div class="copyright"><?= date('Y') . " " .   get_bloginfo('name')  ?>. All rights reserved.</div>
 
