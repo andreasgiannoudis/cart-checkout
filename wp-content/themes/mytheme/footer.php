9be758dff7e0822701da24d4a3645d4752ffac26
Erik Georgievski <erik.georgievski@gritacademy.se>
2024-02-14 21:30:27 +0100

    I have added dynamic information in the footer about the store.

--- wp-content/themes/mytheme/footer.php
+++ wp-content/themes/mytheme/footer.php
@@ -1,8 +1,38 @@
 <footer>
+    <div class="footer-box">
+        <div class="footer-box1">
+            <h3>Free Delivery</h3>
+            <p>For all oders over $50, consectetur adipim scing elit.</p>
+
+        </div>
+        <div class="footer-box2">
+            <h3>90 Days Return</h3>
+            <p>If goods have problems, consectetur adipim scing elit.</p>
+
+        </div>
+        <div class="footer-box3">
+            <h3>Secure Payment</h3>
+            <p>100% secure payment, consectetur adipim scing elit.</p>
+
+        </div>
+    </div>
     <div class="footer-left">
     </div>
 
     <div class="footer-center">
+        <div class="store-info">
+        
+        <p>
+             <?php echo get_bloginfo('name'); ?>
+            <?php echo get_option('woocommerce_store_city') . "," ?> <br>
+            <?php echo get_option('woocommerce_store_postcode'); ?>
+            <?php echo get_option('woocommerce_default_country'); ?>
+
+        </p>
+
+
+
+        </div>
         <?php
 
         $menu_header = array(
@@ -31,6 +61,7 @@
         ?>
 
     </div>
+
     <div class="copyright"><?= date('Y') . " " .   get_bloginfo('name')  ?>. All rights reserved.</div>
 
 </footer>
